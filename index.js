ba05e8e70417418efb7df7a2390c06a7fa0aae12
David <davidchedrick@gmail.com>
2021-10-04 00:35:16 -0500

    starting js added

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
-function init() {
-    
+class matchCats {
+    constructor(time, cards) {
+        this.cardsArray = cards;
+        this.timeCount = time;
+        this.timeLeft = time;
+        this.timer = document.querySelector('#time');
+        this.score = document.querySelector('#result');
+    }
+    playGame() {
+        this.cardSelected = null;
+        this.totalClicks = 0;
+        this.timeLeft = this.time;
+        this.matchedCards = [];
+        this.stop = true;
+    }
+    flipCard(card) {
+        if(this.canFlipCard(card)) {
+            this.totalClicks++;
+            this.score.innerText = `${this.totalClicks}`;
+            card.classList.add('visible')
+        }
+    }
+    canFlipCard(card) {
+        return true;
+        // return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardSelected);
+    }
 }
-init()
+
+
+
+function startGame() {
+    let screenText = document.querySelector('.screenText');
+    let cards = Array.from(document.querySelectorAll('.card'))
+    let game = new matchCats(100, cards)
+
+    screenText.addEventListener('click', () => {
+        screenText.classList.remove('visible');
+        game.playGame
+    });
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            game.flipCard(card)
+        })
+    })
+}        
+startGame(); 
+
+
+   
+
+
