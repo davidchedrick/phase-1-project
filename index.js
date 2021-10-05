067f6c6384281339d4d864b9f5fa6655beaa2193
David <davidchedrick@gmail.com>
2021-10-05 10:54:39 -0500

    cleaned up JS

--- index.js
+++ index.js
@@ -1,135 +1,135 @@
-class matchCats {
-    constructor(time, cards) {
-        this.cardsArray = cards;
-        this.timeCount = time;
-        this.timeLeft = time;
-        this.timer = document.querySelector('#time');
-        this.score = document.querySelector('#result');
-     
-    }
-    playGame() {
-        this.cardSelected = null;
-        this.totalClicks = 0;
-        this.timer = this.timeLeft;
-        this.matchedCards = [];
-        this.busy = true;
+let screenText = document.querySelector('.screenText');
+let cards = Array.from(document.querySelectorAll('.card'))
+let cardsArray = cards;
+let timeCount = null;
+let timeLeft = 10;
+let timer = document.querySelector('#time');
+let score = document.querySelector('#result');
+let cardSelected = null;
+let totalClicks = 0;
+let matchedCards = [];
+let busy = true;
 
-        setTimeout(() => {
-            this.mixCards(this.cardsArray);
-            this.countDown = this.startCountDown();
-            this.busy = false;
-        }, 500)
-        this.hideCards();
-        this.timer = document.querySelector('#time');
-        this.timer.innerText = this.timeLeft;
-        this.score.innerText = this.totalClicks;
-    }
-    hideCards() {
-        this.cardsArray.forEach(card => {
-            card.classList.remove('visible');
-            card.classList.remove('matched');
-        });
-    }
-    mixCards(cardsArray){
-        for(let i = this.cardsArray.length - 1; i > 0; i--){
-            let mixIndex = Math.floor(Math.random() * (i + 1));
-            this.cardsArray[mixIndex].style.order = i;
-            this.cardsArray[i].style.order = mixIndex;
-        }
-    }
-    startCountDown() {
-        return setInterval(() => {
-            this.timer = document.querySelector('#time');
-            this.timeLeft--;
-            this.timer.innerText = this.timeLeft;
-            
-            if(this.timeLeft === 0){
-                this.gameOver();
-            }
-        }, 1000)
-    }
-    flipCard(card) {
-        if(this.canFlipCard(card)) {
-            this.totalClicks++;
-            this.score.innerText = this.totalClicks;
-            card.classList.add('visible');
-
-            if(this.cardSelected){
-                this.checkMatch(card);
-            } else {
-                    this.cardSelected = card;
-                }
-            
-        }
-    }
-    checkMatch(card){
-        if(this.cardType(card) === this.cardType(this.cardSelected)){
-            this.cardMatch(card, this.cardSelected);
-        }else{
-            this.misMatch(card, this.cardSelected);
-        }
-        this.cardSelected = null;
-    }
-    cardMatch(card1, card2){
-        this.matchedCards.push(card1);
-        this.matchedCards.push(card2);
-        card1.classList.add('matched');
-        card2.classList.add('matched');
-        if(this.matchedCards.length === this.cardsArray.length) {
-            this.winGame();
-        }
-    }
-    misMatch(card1, card2) {
-        this.busy = true;
-        setTimeout(() => {
-            card1.classList.remove('visible');
-            card2.classList.remove('visible');
-            this.busy = false;
-        }, 1000);
-    }
-    cardType(card){
-        return card.querySelectorAll('.cat')[0].src
-    }
-    canFlipCard(card) {
-       return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardSelected);
-    }
-    gameOver() {
-        clearInterval(this.countDown);
-        document.querySelector('#gameOver').classList.add('visible');
-        document.querySelector('#gameOver').addEventListener('click', () => {
-            screenText.classList.remove('visible');
-            new matchCats(10, cards).playGame()
-        });
-        
-    }
-    winGame() {
-        clearInterval(this.countDown);
-        document.querySelector('#win').classList.add('visible');
-        
-    }
-}
-
-
+document.addEventListener('DomContentLoaded', startGame())
 
 function startGame() {
-    let screenText = document.querySelector('.screenText');
-    let cards = Array.from(document.querySelectorAll('.card'))
-    let game = new matchCats(10, cards)
 
-    screenText.addEventListener('click', () => {
+        screenText.addEventListener('click', () => {
         screenText.classList.remove('visible');
-        game.playGame()
+        playGame()
     });
 
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            game.flipCard(card)
+            flipCard(card)
         })
     })
 }        
-startGame(); 
 
 
+
+function playGame() {
+    setTimeout(() => {
+        mixCards(cardsArray);
+        countDown = startCountDown();
+        busy = false;
+    },500)
+    hideCards();
+    timer = document.querySelector('#time');
+    timer.innerText = timeLeft;
+    score.innerText = totalClicks;
+}
+
+function hideCards() {
+    cardsArray.forEach(card => {
+        card.classList.remove('visible');
+        card.classList.remove('matched');
+    });
+}
+
+function mixCards(cardsArray){
+    for(let i = cardsArray.length - 1; i > 0; i--){
+        let mixIndex = Math.floor(Math.random() * (i + 1));
+        cardsArray[mixIndex].style.order = i;
+        cardsArray[i].style.order = mixIndex;
+    }
+}
+
+function startCountDown() {
+    return setInterval(() => {
+        timer = document.querySelector('#time');
+        timeLeft--;
+        timer.innerText = timeLeft;
+        console.log(timer)
+        if(timeLeft === 0){
+            gameOver();
+        }
+    }, 1000)
+}
+
+function flipCard(card) {
+    if(canFlipCard(card)) {
+        totalClicks++;
+        score.innerText = totalClicks;
+        card.classList.add('visible');
+
+        if(cardSelected){
+            checkMatch(card);
+        } else {
+            cardSelected = card;
+        }
+    }
+}
+
+function checkMatch(card){
+    if(cardType(card) === cardType(cardSelected)){
+        cardMatch(card, cardSelected);
+    }else{
+        misMatch(card, cardSelected);
+    }
+    cardSelected = null;
+}
+
+function cardMatch(card1, card2){
+    matchedCards.push(card1);
+    matchedCards.push(card2);
+    card1.classList.add('matched');
+    card2.classList.add('matched');
+    if(matchedCards.length === cardsArray.length) {
+        winGame();
+    }
+}
+
+function misMatch(card1, card2) {
+    busy = true;
+    setTimeout(() => {
+        card1.classList.remove('visible');
+        card2.classList.remove('visible');
+        busy = false;
+    }, 1000);
+}
+
+function cardType(card){
+    return card.querySelectorAll('.cat')[0].src
+}
+
+function canFlipCard(card) {
+   return (!busy && !matchedCards.includes(card) && card !== cardSelected);
+}
+
+function gameOver() {
+    clearInterval(countDown);
+    document.querySelector('#gameOver').classList.add('visible');
+
+
+    
+
+}
+function winGame() {
+    clearInterval(countDown);
+    document.querySelector('#win').classList.add('visible');
+
+}
    
 
 
