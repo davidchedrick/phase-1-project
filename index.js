c42bba0dd1f288f8e26616094655ed37d5770ec8
David <davidchedrick@gmail.com>
2021-10-11 21:57:44 -0500

    cleaned up JS

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 let screenText = document.querySelector('.screenText');
-let cards = Array.from(document.querySelectorAll('.card'))
+let cards = Array.from(document.querySelectorAll('.card'));
 let cardsArray = cards;
 let timeCount = null;
 let timeLeft = 60;
 let timer = document.querySelector('#time');
 let score = document.querySelector('#result');
 let cardSelected = null;
-let totalClicks = 0;
+let totalPoints = 0;
 let matchedCards = [];
 let busy = true;
 
 document.addEventListener('DomContentLoaded', startGame())
 
 function startGame() {
-    
-
-        screenText.addEventListener('click', () => {
-        screenText.classList.remove('visible');
-        playGame()
+    screenText.addEventListener('click', () => {
+    screenText.classList.remove('visible');
+    playGame();
     });
 
     cards.forEach(card => {
@@ -27,18 +25,16 @@ function startGame() {
     })
 }        
 
-
-
 function playGame() {
     setTimeout(() => {
         mixCards(cardsArray);
         countDown = startCountDown();
         busy = false;
-    },500)
+    }, 500),
     hideCards();
     timer = document.querySelector('#time');
     timer.innerText = timeLeft;
-    score.innerText = totalClicks;
+    score.innerText = totalPoints;
 }
 
 function hideCards() {
@@ -48,8 +44,8 @@ function hideCards() {
     });
 }
 
-function mixCards(cardsArray){
-    for(let i = cardsArray.length - 1; i > 0; i--){
+function mixCards(cardsArray) {
+    for(let i = cardsArray.length - 1; i > 0; i--) {
         let mixIndex = Math.floor(Math.random() * (i + 1));
         cardsArray[mixIndex].style.order = i;
         cardsArray[i].style.order = mixIndex;
@@ -61,11 +57,10 @@ function startCountDown() {
         timer = document.querySelector('#time');
         timeLeft--;
         timer.innerText = timeLeft;
-        console.log(timer)
         if(timeLeft === 0){
             gameOver();
         }
-    }, 1000)
+    }, 1000);
 }
 
 function flipCard(card) {
@@ -73,7 +68,7 @@ function flipCard(card) {
        
         card.classList.add('visible');
 
-        if(cardSelected){
+        if(cardSelected) {
             checkMatch(card);
         } else {
             cardSelected = card;
@@ -81,22 +76,22 @@ function flipCard(card) {
     }
 }
 
-function checkMatch(card){
-    if(cardType(card) === cardType(cardSelected)){
+function checkMatch(card) {
+    if(cardType(card) === cardType(cardSelected)) {
         cardMatch(card, cardSelected);
-    }else{
+    } else {
         misMatch(card, cardSelected);
     }
     cardSelected = null;
 }
 
-function cardMatch(card1, card2){
+function cardMatch(card1, card2) {
     matchedCards.push(card1);
     matchedCards.push(card2);
     card1.classList.add('matched');
     card2.classList.add('matched');
-    totalClicks++;
-    score.innerText = totalClicks;
+    totalPoints++;
+    score.innerText = totalPoints;
     if(matchedCards.length === cardsArray.length) {
         winGame();
     }
@@ -111,8 +106,8 @@ function misMatch(card1, card2) {
     }, 1000);
 }
 
-function cardType(card){
-    return card.querySelectorAll('.cat')[0].src
+function cardType(card) {
+    return card.querySelectorAll('.cat')[0].src;
 }
 
 function canFlipCard(card) {
@@ -121,28 +116,22 @@ function canFlipCard(card) {
 
 function gameOver() {
     clearInterval(countDown);
-    const gameOver = document.querySelector('#gameOver')
+    const gameOver = document.querySelector('#gameOver');
     gameOver.classList.add('visible');
-    gameOver.addEventListener('click', reload)
-
-    
-
+    gameOver.addEventListener('click', reload);
 }
+
 function winGame() {
     clearInterval(countDown);
-    const win = document.querySelector('#win')
+    const win = document.querySelector('#win');
     win.classList.add('visible');
-    win.addEventListener('click', reload)
-
+    win.addEventListener('click', reload);
 }
    
 function reload(){
     location.reload();
- }   
+}   
    
-  
-
-
 function fetchCats() {
     fetch('https://catfact.ninja/fact')
     .then(response => response.json())
@@ -151,16 +140,11 @@ function fetchCats() {
 }
 fetchCats()
   
-
-
-
 function renderCats(cat) {
-    console.log(cat.fact)
     const main = document.querySelector('#facts');
     const h2 = document.createElement('h2');
     h2.innerHTML = cat.fact;
-    main.appendChild(h2);
-    
+    main.append(h2);
 }
 
  
