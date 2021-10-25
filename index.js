0910b3712f257288b6b14832e7221de0161448e6
David <davidchedrick@gmail.com>
2021-10-24 23:41:09 -0500

    added number css, cleaned JS

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-let screenText = document.querySelector('.screenText');
-let cards = Array.from(document.querySelectorAll('.card'));
-let cardsArray = cards;
-let timeCount = null;
-let timeLeft = 60;
-let timer = document.querySelector('#time');
-let score = document.querySelector('#result');
+const screenText = document.querySelector('.screenText');
+const cards = document.querySelectorAll('.card');
+const timer = document.querySelector('#time');
+const score = document.querySelector('#result');
 let cardSelected = null;
+let timeLeft = 60;
 let totalPoints = 0;
 let matchedCards = [];
 let busy = true;
 
-document.addEventListener('DomContentLoaded', startGame())
+// Game Start
+
+document.addEventListener('DOMContentLoaded', startGame())
 
 function startGame() {
     screenText.addEventListener('click', () => {
@@ -18,45 +18,34 @@ function startGame() {
     playGame();
     });
 
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            flipCard(card)
-        })
-    })
+    cards.forEach(card => card.addEventListener('click', () => flipCard(card)));
 }        
 
 function playGame() {
     setTimeout(() => {
-        mixCards(cardsArray);
+        mixCards(cards);
         countDown = startCountDown();
         busy = false;
-    }, 500),
-    hideCards();
-    timer = document.querySelector('#time');
-    timer.innerText = timeLeft;
-    score.innerText = totalPoints;
+    }, 100),
+
+   timer.style.color = 'red'
+
 }
 
-function hideCards() {
-    cardsArray.forEach(card => {
-        card.classList.remove('visible');
-        card.classList.remove('matched');
-    });
-}
-
-function mixCards(cardsArray) {
-    for(let i = cardsArray.length - 1; i > 0; i--) {
+function mixCards(cards) {
+    for(let i = cards.length - 1; i > 0; i--) {
         let mixIndex = Math.floor(Math.random() * (i + 1));
-        cardsArray[mixIndex].style.order = i;
-        cardsArray[i].style.order = mixIndex;
+        cards[mixIndex].style.order = i;
+        cards[i].style.order = mixIndex;
     }
 }
 
-function startCountDown() {
+const startCountDown = () => {
     return setInterval(() => {
-        timer = document.querySelector('#time');
-        timeLeft--;
+        timeLeft--; 
         timer.innerText = timeLeft;
+        timer.style.color = timeLeft % 2 === 0 ? "red" : "blue";
+
         if(timeLeft === 0){
             gameOver();
         }
@@ -76,6 +65,10 @@ function flipCard(card) {
     }
 }
 
+function canFlipCard(card) {
+   return (!busy && !matchedCards.includes(card) && card !== cardSelected);
+}
+
 function checkMatch(card) {
     if(cardType(card) === cardType(cardSelected)) {
         cardMatch(card, cardSelected);
@@ -85,6 +78,10 @@ function checkMatch(card) {
     cardSelected = null;
 }
 
+function cardType(card) {
+    return card.querySelectorAll('.cat')[0].src;
+}
+
 function cardMatch(card1, card2) {
     matchedCards.push(card1);
     matchedCards.push(card2);
@@ -92,7 +89,8 @@ function cardMatch(card1, card2) {
     card2.classList.add('matched');
     totalPoints++;
     score.innerText = totalPoints;
-    if(matchedCards.length === cardsArray.length) {
+
+    if(matchedCards.length === cards.length) {
         winGame();
     }
 }
@@ -106,13 +104,7 @@ function misMatch(card1, card2) {
     }, 1000);
 }
 
-function cardType(card) {
-    return card.querySelectorAll('.cat')[0].src;
-}
-
-function canFlipCard(card) {
-   return (!busy && !matchedCards.includes(card) && card !== cardSelected);
-}
+//End Game
 
 function gameOver() {
     clearInterval(countDown);
@@ -132,6 +124,8 @@ function reload(){
     location.reload();
 }   
    
+// API Cat Facts
+
 function fetchCats() {
     fetch('https://catfact.ninja/fact')
     .then(response => response.json())
@@ -147,4 +141,6 @@ function renderCats(cat) {
     main.append(h2);
 }
 
- 
+
+   
+
